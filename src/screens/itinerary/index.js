ddb08c26cecf2aec4a1ca3fd50e0e809272eec27
yippira <yippygames@gmail.com>
2020-07-05 00:55:10 +0800

    chore: add add itinerary button

--- src/screens/itinerary/index.js
+++ src/screens/itinerary/index.js
@@ -6,6 +6,10 @@ import Button from '@material-ui/core/Button';
 import events from './data';
 import {Link} from 'react-router-dom';
 import coverPhoto from '../../assets/images/brooklyn.jpeg';
+
+import AddIcon from '@material-ui/icons/Add';
+
+
 const Container = styled.div`
   display: grid;
   justify-content: center;
@@ -60,6 +64,18 @@ const StyledH2 = styled.h2`
 const StyledH3 = styled.h3`
     color: #fff;
 `
+const HeaderActions = styled.div`
+    position absolute;
+    top: 80%;
+    display: grid;
+    left:78%;
+`
+
+const AddButton = styled(Button)`
+    border-color: white !important;
+    color: white !important;
+`
+
 
 const Itinerary = () => {
     return (
@@ -71,6 +87,9 @@ const Itinerary = () => {
                 <StyledH2>Jul 5 - Jul 7, 2020</StyledH2>
                 <StyledH3>New York City, USA</StyledH3>
             </HeaderTextContainer>
+            <HeaderActions>
+                <AddButton variant="outlined">Add Itinerary <AddIcon/></AddButton>
+            </HeaderActions>
         </Header>
         <Container>
             <StyledLink to="/dashboard">
